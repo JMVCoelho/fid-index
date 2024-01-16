6da06809209495fca5a72c96440b16001e3a83a4
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2024-01-16 15:17:23 +0000

    add new data scripts

--- scripts/marco_documents/build_data_fusion_tokenizer.py
+++ scripts/marco_documents/build_data_fusion_tokenizer.py
@@ -1,12 +1,24 @@
 # Given the data from here: https://github.com/thunlp/OpenMatch/blob/fbf198ad5353e35b8b67ea7539783db61b32d1f2/v1/docs/experiments-msmarco-doc.md
 # build files files in normalized format
 
+# this uses a tokenizer instead of sentence level segmentation.
 
 import re
 
 import pickle
 
-MAX_DOC_LEN = 128
+import sys
+
+from transformers import AutoTokenizer
+
+from tqdm import tqdm
+
+tokenizer = AutoTokenizer.from_pretrained("/user/home/jcoelho/clueweb-structured-retrieval/models/t5-base-scaled")
+
+MAX_DOC_LEN = sys.argv[1] # 512
+N_PASSAGES = sys.argv[2] # 4
+
+corpus_only = True
 
 def is_valid_id(did):
     pattern = re.compile(r'^D\d+$')
@@ -14,12 +26,21 @@ def is_valid_id(did):
 
 ID_MAPPER_DOCS = {}
 
-corpus_only = False
+import re
+
+def split_list(lst, n):
+    quotient = len(lst) // n
+    remainder = len(lst) % n
+
+    divided_list = [lst[i * quotient + min(i, remainder):(i + 1) * quotient + min(i + 1, remainder)] for i in range(n)]
+    
+    return divided_list
+
 
 with open("/user/home/jcoelho/clueweb-structured-retrieval/marco/documents/msmarco-docs.tsv", "r") as f, \
-    open(f"/user/home/jcoelho/clueweb-structured-retrieval/marco/documents/corpus_firstp_{MAX_DOC_LEN}.tsv", "w") as fout:
+    open(f"/user/home/jcoelho/clueweb-structured-retrieval/marco/documents/corpus_{N_PASSAGES}p_{MAX_DOC_LEN}.tsv", "w") as fout:
     start_id = 0
-    for line in f:
+    for line in tqdm(f, total=3213835):
         try:
             did, url, title, text = line.strip().split("\t")
         except Exception:
@@ -41,12 +62,23 @@ with open("/user/home/jcoelho/clueweb-structured-retrieval/marco/documents/msmar
         ID_MAPPER_DOCS[did] = str(start_id)
 
         text = text.replace("\"", "").replace("\'", "").replace("\n", "").replace("\t", "").replace("\r", "")
-        text = " ".join(text.split(" ")[:MAX_DOC_LEN])
+        
+        tokenized_text = tokenizer.encode(text)[:-1]
+
+        splits = split_list(tokenized_text, int(N_PASSAGES))
+
+        split_text = tokenizer.batch_decode(splits)
+
+        text = '[PSEP]'.join(split_text)
+
+        assert len(text.split('[PSEP]')) == int(N_PASSAGES)
+
         fout.write(f"{ID_MAPPER_DOCS[did]}\t{title}\t{text}\n")
         start_id += 1
 
 if corpus_only:
-    exit()
+    exit(0)
+
 
 # train.negatives.tsv <qid>\t<neg1,neg2,neg3...>
 negatives = {}
@@ -74,7 +106,7 @@ with open("marco/documents/train.negatives.tsv", 'w') as fout:
         fout.write(f"{qid}\t{negs}\n")
 
 
-train_queries_with_qrels = []
+
 with open("marco/documents/msmarco-doctrain-qrels.tsv", 'r') as f, \
     open("marco/documents/qrels.train.tsv", 'w') as fout:
 
@@ -86,15 +118,9 @@ with open("marco/documents/msmarco-doctrain-qrels.tsv", 'r') as f, \
                 did = ID_MAPPER_DOCS[did[1:]]
             else:
                 raise Exception("problem with ids: train qrels")
-
-            train_queries_with_qrels.append(qid)
         
             fout.write(f"{qid}\t{z}\t{did}\t{rel}\n")
 
-train_queries_with_qrels = set(train_queries_with_qrels)
-
-
-dev_queries_with_qrels = []
 with open("marco/documents/msmarco-docdev-qrels.tsv", 'r') as f, \
     open("marco/documents/qrels.dev.tsv", 'w') as fout:
 
@@ -105,26 +131,35 @@ with open("marco/documents/msmarco-docdev-qrels.tsv", 'r') as f, \
             did = ID_MAPPER_DOCS[did[1:]]
         else:
             raise Exception("problem with ids: dev qrels")
-
-        dev_queries_with_qrels.append(qid)
+    
         fout.write(f"{qid}\t{z}\t{did}\t{rel}\n")
 
-dev_queries_with_qrels = set(dev_queries_with_qrels)
 
+with open("/user/home/jcoelho/clueweb-structured-retrieval/marco/documents/msmarco-docs.tsv", "r") as f, \
+    open("/user/home/jcoelho/clueweb-structured-retrieval/marco/documents/corpus.tsv", "w") as fout:
 
-with open("marco/documents/train.query.txt", 'r') as f, \
-    open("marco/documents/train.query.filtered.txt", 'w') as fout:
     for line in f:
-        qid, text = line.strip().split("\t")
-        if qid in train_queries_with_qrels:
-            fout.write(f"{qid}\t{text}\n")
+        try:
+            did, url, title, text = line.strip().split("\t")
+        except Exception:
+            if len(line.strip().split("\t")) == 3:
+                did = line.strip().split("\t")[0]
+                title = "-"
+                text = line.strip().split("\t")[1]
+            if len(line.strip().split("\t")) == 2:
+                did = line.strip().split("\t")[0]
+                title = "-"
+                text = line.strip().split("\t")[-1]
+            if len(line.strip().split("\t")) == 1:
+                raise Exception("this should not happen if initial dataset is correct, check it")
+        if is_valid_id(did):
+            did = ID_MAPPER_DOCS[did[1:]]
+        else:
+            raise Exception("problem with ids: corpus")
 
-with open("marco/documents/dev.query.txt", 'r') as f, \
-    open("marco/documents/dev.query.filtered.txt", 'w') as fout:
-    for line in f:
-        qid, text = line.strip().split("\t")
-        if qid in dev_queries_with_qrels:
-            fout.write(f"{qid}\t{text}\n")
+        text = text.replace("\"", "").replace("\'", "").replace("\n", "").replace("\t", "").replace("\r", "")
+        fout.write(f"{did}\t{title}\t{text}\n")
 
+        
 with open('marco/documents/id2seqint_mapper.tsv', 'wb') as h:
     pickle.dump(ID_MAPPER_DOCS, h, protocol=pickle.HIGHEST_PROTOCOL)
